aed917c5f2fa8a8f4166976136cb6ae1a74c2e6f
ankita21gaikwad <96906679+ankita21gaikwad@users.noreply.github.com>
2022-06-09 20:02:23 +0530

    change title message

--- src/models/playwright-home.page.modal.ts
+++ src/models/playwright-home.page.modal.ts
@@ -1,4 +1,4 @@
 export class PlaywrightHomePageModal {
-    static playwrightHomePageTitle: string = 'Getting started | Playwright';
+    static playwrightHomePageTitle: string = 'Fast and reliable end-to-end testing for modern web apps | Playwright';
 
 }
